61f2ebb38c27715116635cb26814e26bce786365
Carlton Mason <carlton.mason@gmail.com>
2017-05-16 11:28:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -136,3 +136,10 @@ var main = {
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
+
+$(document).ready(function() {
+	$('.opener').on('click', function() {
+		var target = $(this).dataset.target;
+		$(target).addClass('active');
+	});
+});
